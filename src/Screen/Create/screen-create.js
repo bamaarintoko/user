e19f8574dcefea2d80410f1d56a9688bdd1fc24e
bama <yodafatbama.arintko@gmail.com>
2018-11-08 16:43:55 +0700

    asd

--- src/Screen/Create/screen-create.js
+++ src/Screen/Create/screen-create.js
@@ -1,14 +1,49 @@
 import React, {Component} from "react"
-import {Container, Text, Content, List, ListItem, Left, Body, Right,Button} from "native-base"
+import {Container, Text, Content, List, ListItem, Left, Body, Right, Button} from "native-base"
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {Header} from "../../Component/Header"
-import {StyleSheet, View, Dimensions} from "react-native"
+import {StyleSheet, View, Dimensions, TouchableWithoutFeedback} from "react-native"
 import {connect} from "react-redux";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Modal from "react-native-modal";
+
 let {width, height} = Dimensions.get('window')
 
 class ScreenCreate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalVisible: false,
+            isModalDropOff: false
+        }
+    }
+
+    _toggleModal = () => {
+
+        return () => {
+
+            this.setState({isModalVisible: !this.state.isModalVisible});
+        }
+
+    }
+    toggleDropOff = () => {
+        return () => {
+            this.setState({
+                isModalDropOff: !this.state.isModalDropOff
+            })
+        }
+    }
+    onKeyBack = () => {
+        return () => {
+            console.log("asdf")
+            this.setState({
+                isModalVisible: false,
+                isModalDropOff: false
+            })
+        }
+    }
+
     render() {
         return (
             <Container>
@@ -26,22 +61,39 @@ class ScreenCreate extends Component {
                     >
                     </MapView>
                 </View>
+                {/*MODAL PICK UP*/}
+                <Modal isVisible={this.state.isModalVisible} onBackButtonPress={this.onKeyBack()}>
+                    <View style={{flex: 1, backgroundColor: "#FFF"}}>
+                        <Text>I am the modal Pick Up!</Text>
+                    </View>
+                </Modal>
+                {/*END MODAL*/}
+                {/*MODAL DROP OFF*/}
+                <Modal isVisible={this.state.isModalDropOff} onBackButtonPress={this.onKeyBack()}>
+                    <View style={{flex: 1, backgroundColor: "#FFF"}}>
+                        <Text>I am the modal Drop Off!</Text>
+                    </View>
+                </Modal>
+                {/*END MODAL*/}
                 <Content style={{margin: 15}}>
                     <List>
-                        <ListItem avatar>
-                            <Left>
-                                <View style={styles.icn}>
-                                    <Icon name={"street-view"} size={wp("5%")} color={"#D50000"}/>
-                                </View>
-                            </Left>
-                            <Body>
-                            <Text style={styles.text_title}>Pick Up Location</Text>
-                            <Text style={styles.text_note} note>Rp 500.000</Text>
-                            </Body>
-                            <Right>
-                                <Icon name={"chevron-right"} size={wp("5%")} color={"#D50000"}/>
-                            </Right>
-                        </ListItem>
+                        <TouchableWithoutFeedback onPress={this._toggleModal()}>
+                            <ListItem avatar>
+                                <Left>
+                                    <View style={styles.icn}>
+                                        <Icon name={"street-view"} size={wp("5%")} color={"#D50000"}/>
+                                    </View>
+                                </Left>
+                                <Body>
+                                <Text style={styles.text_title}>Pick Up Location</Text>
+                                <Text style={styles.text_note} note>Rp 500.000</Text>
+                                </Body>
+                                <Right>
+                                    <Icon name={"chevron-right"} size={wp("5%")} color={"#D50000"}/>
+                                </Right>
+                            </ListItem>
+                        </TouchableWithoutFeedback>
+                        <TouchableWithoutFeedback onPress={this.toggleDropOff()}>
                         <ListItem avatar>
                             <Left>
                                 <View style={styles.icn}>
@@ -56,6 +108,7 @@ class ScreenCreate extends Component {
                                 <Icon name={"chevron-right"} size={wp("5%")} color={"#D50000"}/>
                             </Right>
                         </ListItem>
+                        </TouchableWithoutFeedback>
                         <ListItem avatar>
                             <Left>
                                 <View style={styles.icn}>
@@ -127,7 +180,7 @@ class ScreenCreate extends Component {
                             </Right>
                         </ListItem>
                     </List>
-                    <Button block style={{backgroundColor:"#D50000", marginTop:10}}>
+                    <Button block style={{backgroundColor: "#D50000", marginTop: 10}}>
                         <Text>Create Order</Text>
                     </Button>
                 </Content>
@@ -137,11 +190,11 @@ class ScreenCreate extends Component {
 }
 
 const styles = StyleSheet.create({
-    text_title:{
-      fontSize: hp("2.8%")
+    text_title: {
+        fontSize: hp("2.8%")
     },
-    text_note:{
-      fontSize: hp("2.5%")
+    text_note: {
+        fontSize: hp("2.5%")
     },
     icn: {
         width: wp("7%"), justifyContent: 'center', alignItems: 'center'
